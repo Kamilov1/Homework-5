a06d342dd6330ab3324927f97154a1202a9d798d
Kamilov1 <itprofessiya1@gmail.com>
2022-08-11 22:14:53 +0300

    Commit one

---
+++ Program.cs
@@ -0,0 +1,104 @@
+﻿void Zadacha34()
+{
+    //Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. 
+    //Напишите программу, которая покажет количество чётных чисел в массиве.
+
+    Console.Write("Цифра: ");
+    int size = Convert.ToInt32(Console.ReadLine());
+    int[] array = new int[size];
+
+    FillArray(array);
+    PrintArray(array);
+
+    void PrintArray(int[] array)
+    {
+        int size = array.Length;
+        for (int i = 0; i < array.Length; i++)
+        {
+            Console.Write(array[i] + ", ");
+        }
+        Console.WriteLine();
+    }
+
+    void FillArray(int[] array)
+    {
+        int size = array.Length;
+        Random random = new Random();
+
+        for (int i = 0; i < size; i++)
+        {
+            array[i] = random.Next(100, 1000);
+        }
+    }
+}
+Zadacha34();
+
+void Zadacha36()
+{
+    //Задача 36: Задайте одномерный массив, заполненный случайными числами. 
+    //Найдите сумму элементов, стоящих на нечётных позициях.
+
+    Console.Write("Цифра: ");
+    int size = Convert.ToInt32(Console.ReadLine());
+    int[] array = new int[size];
+
+    FillArray(array);
+    PrintArray(array);
+
+    void PrintArray(int[] array)
+    {
+        int size = array.Length;
+        for (int i = 0; i < array.Length; i++)
+        {
+            Console.Write(array[i] + ", ");
+        }
+        Console.WriteLine();
+    }
+
+    void FillArray(int[] array)
+    {
+        int size = array.Length;
+        Random random = new Random();
+
+        for (int i = 0; i < size; i++)
+        {
+            array[i] = random.Next(0, 20);
+        }
+    }
+}
+//Zadacha36();
+
+void Zadacha38()
+{
+    //Задача 38: Задайте массив вещественных чисел. 
+    //Найдите разницу между максимальным и минимальным элементов массива.
+
+    Console.Write("Цифра: ");
+    int size = Convert.ToInt32(Console.ReadLine());
+    int[] array = new int[size];
+
+    FillArray(array);
+    PrintArray(array);
+
+    void PrintArray(int[] array)
+    {
+        int size = array.Length;
+        for (int i = 0; i < array.Length; i++)
+        {
+            Console.Write(array[i] + ", ");
+        }
+        Console.WriteLine();
+    }
+
+    void FillArray(int[] array)
+    {
+        int size = array.Length;
+        Random random = new Random();
+
+        for (int i = 0; i < size; i++)
+        {
+            array[i] = random.Next(0, 10);
+        }
+    }
+}
+//Zadacha38();
